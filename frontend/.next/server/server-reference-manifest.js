29c8c4d35cc26e72c86d2eac098b07b6bfe84ff3
DCUIT <20521120@gm.uit.edu.vn>
2026-05-16 17:41:38 +0700

    chore(frontend): add CSS typings and enable class-based dark mode

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"mdOpVdAqeyWByQtUgcOaujMEevGPBfd87/hYUVz0qvY=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"sGDGcznWDNFzYK0vQNQf60dVemWsx8bH9e25Mpb2TVQ=\"\n}"
